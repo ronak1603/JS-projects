f86ca4891b8c2cc030012c22431defaf12f8be51
ronak1603 <ronakmalani2002@gmail.com>
2024-07-06 00:17:24 +0530

    bug fix: code missing

--- color-game/script.js
+++ color-game/script.js
@@ -3,7 +3,7 @@ function randColorNum() {
 }
 
 function randomRGB() {
-  return `rgb(${randColorNum()},${randColorNum()},${randColorNum()})`;
+  return `rgb(${randColorNum()}, ${randColorNum()}, ${randColorNum()})`;
 }
 
 function generateColors() {
@@ -16,7 +16,6 @@ function generateColors() {
 
 function showBgColor() {
   let colors = generateColors();
-  console.log();
   var randomColorIndex = Math.floor(Math.random() * colors.length);
   const navValue = document.querySelector(".rgb-value");
   navValue.textContent = colors[randomColorIndex];
@@ -26,7 +25,7 @@ function showBgColor() {
     boxes[index].style.backgroundColor = color;
 
     boxes[index].addEventListener("click", () => {
-      if (color == navValue.textContent) {
+      if (color === navValue.textContent) {
         for (let i = 0; i < boxes.length; i++) {
           boxes[i].style.backgroundColor = navValue.textContent;
           navValue.style.backgroundColor = navValue.textContent;
@@ -42,13 +41,27 @@ const resetbutton = document.querySelector(".reset");
 
 resetbutton.addEventListener("click", () => {
   let colors = generateColors();
+
   var randomColorIndex = Math.floor(Math.random() * colors.length);
+
   const navValue = document.querySelector(".rgb-value");
   navValue.style.backgroundColor = "white";
   navValue.textContent = colors[randomColorIndex];
   const boxes = document.querySelectorAll(".color-div");
   colors.forEach((color, index) => {
     boxes[index].style.backgroundColor = color;
+
+    boxes[index].addEventListener("click", () => {
+      if (color === navValue.textContent) {
+        for (let i = 0; i < boxes.length; i++) {
+          boxes[i].style.backgroundColor = navValue.textContent;
+          navValue.style.backgroundColor = navValue.textContent;
+        }
+      } else {
+        boxes[index].style.backgroundColor = "black";
+      }
+    });
   });
 });
+
 showBgColor();
